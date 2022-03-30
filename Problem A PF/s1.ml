ca5029c79f4896a0af5c95a49fe6f7b71118a559
Landeiras <88064946+LuisLandeiras@users.noreply.github.com>
2022-03-30 23:40:15 +0100

    Working on last step

--- Problem A PF/s1.ml
+++ Problem A PF/s1.ml
@@ -5,9 +5,13 @@ let k = ref 0
 let rec s n =
   count := !count + 1;
   match n with
-    0 -> 1
-  | 1 -> 2
-  |_  -> int_of_float((((6. *. (float_of_int (n) -. 2.) +. 9.) /. (float_of_int (n) +. 1.)) *. (float_of_int (s(n-1)))) -. (((float_of_int (n) -. 2.) /. (float_of_int (n) +. 1.)) *. (float_of_int (s(n-2)))))
+    0. -> 1.
+  | 1. -> 2.
+  |_  -> (((6. *. (n -. 2.) +. 9.) /. (n +. 1.)) *. (s(n -. 1.))) -. ((n -. 2.) /. (n +. 1.)) *. ((s(n -. 2.)))
+
+  let aux n = 
+    int_of_float(s(float_of_int (n)))
+  
 
 let rec s1 n =
   count1 := !count1 + 1;
@@ -22,10 +26,18 @@ let rec s1 n =
     done;
     3 * s1(n-1) + !k
 
+open Z
+
+let  rec bigS n =
+  match n with
+    0 -> 1
+  | 1 -> 2
+  |_  -> (((6 * (n - 2) + 9) / (n + 1)) * (bigS(n - 1))) - ((n - 2) / (n + 1)) * ((bigS(n - 2)))
+
 let () =
 for z=0 to 20 do
   print_string "s-> ";
-  print_int (s z);
+  print_int (aux z);
   print_string " count ->";
   print_int !count;
   print_newline ();
@@ -37,4 +49,8 @@ for z=0 to 20 do
   print_newline();
   print_newline();
   count1 := 0
-done
+  
+done;
+print_string "Big S: ";
+print_int (bigS 40);
+print_newline()
